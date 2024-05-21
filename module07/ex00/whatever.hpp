d47d0d63e9401825ac3912618e702b2f1f83ceda
Oussama Kamili <oussama.kamili@protonmail.com>
2024-05-21 19:30:19 +0100

    making module07 ex00 more clear

--- module07/ex00/whatever.hpp
+++ module07/ex00/whatever.hpp
@@ -6,14 +6,14 @@
 /*   By: okamili <okamili@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 08:03:10 by okamili           #+#    #+#             */
-/*   Updated: 2023/12/28 09:38:22 by okamili          ###   ########.fr       */
+/*   Updated: 2024/05/21 19:26:23 by okamili          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef WHATEVER_HPP
 # define WHATEVER_HPP
 
-template <class T>
+template <typename T>
 void swap(T &a, T &b)
 {
 	T tmp;
@@ -23,14 +23,14 @@ void swap(T &a, T &b)
 	b = tmp;
 }
 
-template <class T>
-T min(T a, T b)
+template <typename T>
+T min(T &a, T &b)
 {
 	return ((a < b) ? a : b);
 }
 
-template <class T>
-T max(T a, T b)
+template <typename T>
+T max(T &a, T &b)
 {
 	return ((a > b) ? a : b);
 }
